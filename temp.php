2d4b224ed244697d3dfb10516d486f89d90852ea
Sanjaya Sandakelum <sandakelumsanjaya@gmail.com>
2024-12-25 23:10:46 +0530

    blog list page blog post page things to do page travel information page

---
+++ temp.php
@@ -0,0 +1,60 @@
+<div class="row">
+    <div class="col-md-6">
+        <div class="form-group">
+            <label>Start Date *</label>
+            [text* text-start-date]
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <label>Travel Duration *</label>
+            [text* text-duration]
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="form-group">
+            <label>Adult *</label>
+            [text* text-adult]
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="form-group">
+            <label>Children *</label>
+            [text* text-children]
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <label>Your Country *</label>
+            [text* text-country]
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <label>Your Name *</label>
+            [text* text-name]
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <label>Your Email *</label>
+            [text* text-email]
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <label>Your Mobile *</label>
+            [text* text-mobile]
+        </div>
+    </div>
+    <div class="col-md-6"></div>
+    <div class="col-md-12">
+        <div class="form-group">
+            <label>Message *</label>
+            [textarea text-message]
+        </div>
+    </div>
+    <div class="col-md-12">
+        [submit class:primary-btn class:orange-2 "Send Message"]
+    </div>
+</div>
